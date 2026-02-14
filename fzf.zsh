5ae146b4863043a43033a247a15cef3ca3956adc
Jonathan Branam <jonathan.branam@gmail.com>
2026-02-14 15:25:12 -0500

    Fixes some vim bundles.

--- fzf.zsh
+++ fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/jbranam/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/jbranam/.fzf/bin"
-fi
+# if [[ ! "$PATH" == */Users/jbranam/.fzf/bin* ]]; then
+#   PATH="${PATH:+${PATH}:}/Users/jbranam/.fzf/bin"
+# fi
 
 source <(fzf --zsh)
