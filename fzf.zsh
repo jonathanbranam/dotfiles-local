5d62fc74adcba75fa4cb6cace91ef2738c3dc81e
Jonathan Branam <jonathan.branam@gmail.com>
2026-02-14 15:14:27 -0500

    Various updates.

--- fzf.zsh
+++ fzf.zsh
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */Users/jbranam/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/jbranam/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/Users/jbranam/.fzf/shell/completion.zsh"
-
-# Key bindings
-# ------------
-source "/Users/jbranam/.fzf/shell/key-bindings.zsh"
+source <(fzf --zsh)
